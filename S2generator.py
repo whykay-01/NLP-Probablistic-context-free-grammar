70ae0529c10359bb8aedda49c5ec9819585e920f
Yan <100032599+whykay-01@users.noreply.github.com>
2023-06-13 22:02:29 +0800

    formatted

--- S2generator.py
+++ S2generator.py
@@ -3,21 +3,23 @@
 import re
 
 nonterm = set()
-for s in open('Vocab.gr', 'r'):
+for s in open("Vocab.gr", "r"):
     idx = s.find("#")
-    if idx != -1 :
+    if idx != -1:
         s = s[0:idx]
     if s.strip() != "":
         nonterm.add(s.split()[1])
 
 nonterm = sorted(nonterm)
 
-def write_all(prefix,sep):      
-    print ("1\t"+prefix)
+
+def write_all(prefix, sep):
+    print("1\t" + prefix)
 
     for nt in nonterm:
-        print ("1\t"+prefix+sep+"_"+nt)
+        print("1\t" + prefix + sep + "_" + nt)
 
-write_all("S2","\t")
+
+write_all("S2", "\t")
 for nt in nonterm:
-    write_all("_"+nt+"\t"+nt," ")
+    write_all("_" + nt + "\t" + nt, " ")
